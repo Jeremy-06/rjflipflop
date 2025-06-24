5fc41ce35a1fb0774f512052643fc1619359d2cc
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-06-25 00:41:32 +0800

    LOGIN

--- login.vb
+++ login.vb
@@ -8,19 +8,22 @@ Public Class login
     Public dataReader As SqlDataReader
     Public dataTable As New DataTable
     Dim query As String
+
+    Public customerId As Integer
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim username As String = TextBox1.Text.Trim()
         Dim password As String = TextBox2.Text.Trim()
-        query = "SELECT COUNT(*) FROM users WHERE username = @username AND password = @password"
+        query = "SELECT customer_id FROM users WHERE username = @username AND password = @password"
         Try
             connection.Open()
             command = New SqlCommand(query, connection)
             command.Parameters.AddWithValue("@username", username)
             command.Parameters.AddWithValue("@password", password)
-            Dim userCount As Integer = Convert.ToInt32(command.ExecuteScalar())
-            If userCount > 0 Then
-                MessageBox.Show("Login successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-                ' Proceed to next form or main application
+            Dim customerIdObj As Object = command.ExecuteScalar()
+            If customerIdObj IsNot Nothing AndAlso Not IsDBNull(customerIdObj) Then
+                customerId = Convert.ToInt32(customerIdObj)
+                MessageBox.Show("Login successful! Customer ID: " & customerId.ToString(), "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                ' You can now use customerId as needed in your application
             Else
                 MessageBox.Show("Invalid username or password.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
             End If
@@ -29,5 +32,7 @@ Public Class login
         Finally
             connection.Close()
         End Try
+        Me.Hide()
+        intro.Show()
     End Sub
 End Class
