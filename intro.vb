3ee958ecd471c5ef11e4bda8f97cb000127b570d
Ron Jeremy Primavera <ronjeremy.primavera@tup.edu.ph>
2025-06-26 23:16:43 +0800

    CONNECTION

--- intro.vb
+++ intro.vb
@@ -16,4 +16,8 @@ Public Class intro
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         login.Show()
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        register.Show()
+    End Sub
 End Class
